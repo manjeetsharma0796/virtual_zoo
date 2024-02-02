b0cce10e56988d939434585fba1447fc5dc801e2
monami_nandy <monaminandy16@gmail.com>
2024-02-02 17:59:38 +0530

    add: museum, contact, animals, about html pages

--- src/router.js
+++ src/router.js
@@ -13,12 +13,36 @@ const serveEducation = (req, res) => {
   res.sendFile(filePath);
 }
 
+const serveAbout = (req, res) => {
+  const filePath = `${ROOT_DIR}/public/about.html`;
+  res.sendFile(filePath);
+}
+
+const serveAnimals = (req, res) => {
+  const filePath = `${ROOT_DIR}/public/animals.html`;
+  res.sendFile(filePath);
+}
+
+const serveContact = (req, res) => {
+  const filePath = `${ROOT_DIR}/public/contact.html`;
+  res.sendFile(filePath);
+}
+
+const serveMuseum = (req, res) => {
+  const filePath = `${ROOT_DIR}/public/museum.html`;
+  res.sendFile(filePath);
+}
+
 const createApp = () => {
     // Create a new Express application.
     const app = express();
     
     app.get("/", serveHome);
     app.get("/education", serveEducation);
+    app.get("/about", serveAbout);
+    app.get("/animals", serveAnimals);
+    app.get("/contact", serveContact);
+    app.get("/museum", serveMuseum);
     app.use(express.static("public"));
     return app;
 }
